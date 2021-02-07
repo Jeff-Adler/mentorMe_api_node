05758c0de7eace18d75a05bcdebff77c2cdf2883
Jeff Adler <JeffM.Adler@gmail.com>
2021-02-07 15:23:36 -0500

    sucessfully configured postgresql with TypeORM

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
-// Initialize database & Start express server
-
+// Server imports
 import { environmentVars } from './config/index';
-import 'reflect-metadata';
 import express from 'express';
+
+// Database imports
+import 'reflect-metadata';
+import { createConnection } from 'typeorm';
+import { User } from './entity/User';
 
 const app = express();
 
@@ -10,7 +13,26 @@ app.get('/', (req, res) => {
   res.send('Hello world!');
 });
 
-// start the Express server
+// Start Express server
 app.listen(environmentVars.port, () => {
   console.log(`Server is running on port ${environmentVars.port}`);
 });
+
+// Initialize database connection
+createConnection()
+  .then(async (connection) => {
+    console.log('Inserting a new user into the database...');
+    const user = new User();
+    user.firstName = 'Timber';
+    user.lastName = 'Saw';
+    user.age = 25;
+    await connection.manager.save(user);
+    console.log('Saved a new user with id: ' + user.id);
+
+    console.log('Loading users from the database...');
+    const users = await connection.manager.find(User);
+    console.log('Loaded users: ', users);
+
+    console.log('Here you can setup and run express/koa/any other framework.');
+  })
+  .catch((error) => console.log(error));
